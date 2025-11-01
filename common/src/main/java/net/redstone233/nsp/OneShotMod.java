8913646459dd79c76eccb3a63dd7f277102e86b5
Redstone233 <3356168312@qq.com>
2025-11-01 16:07:21 +0800

    Initial commit

---
+++ common/src/main/java/net/redstone233/nsp/OneShotMod.java
@@ -0,0 +1,9 @@
+package net.redstone233.nsp;
+
+public final class OneShotMod {
+    public static final String MOD_ID = "nsp";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/net/redstone233/nsp/neoforge/OneShotModNeoForge.java
@@ -0,0 +1,12 @@
+package net.redstone233.nsp.neoforge;
+
+import net.redstone233.nsp.OneShotMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(OneShotMod.MOD_ID)
+public final class OneShotModNeoForge {
+    public OneShotModNeoForge() {
+        // Run our common setup.
+        OneShotMod.init();
+    }
+}
