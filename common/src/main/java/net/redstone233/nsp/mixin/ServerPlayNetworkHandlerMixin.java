9cec07e1fd134a016dc73d358db2c02e056ded7b
Redstone233 <3356168312@qq.com>
2025-11-07 07:10:11 +0800

    Initial commit

--- common/src/main/java/net/redstone233/nsp/mixin/ServerPlayNetworkHandlerMixin.java
+++ common/src/main/java/net/redstone233/nsp/mixin/ServerPlayNetworkHandlerMixin.java
@@ -5,29 +5,26 @@ import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.redstone233.nsp.config.ClientConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin {
 
-    @ModifyArg(
+    @Redirect(
             method = "onCreativeInventoryAction",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/item/ItemStack;getCount()I"
             )
     )
-    private ItemStack redirectCountCheck(ItemStack stack) {
+    private int redirectCountCheck(ItemStack stack) {
         int actualCount = stack.getCount();
         int configMax = ClientConfig.getMaxItemStackCount();
         int vanillaMax = stack.getMaxCount();
 
         if (actualCount > vanillaMax && actualCount <= configMax) {
-            // 返回修改后的 ItemStack
-            ItemStack modifiedStack = stack.copy();
-            modifiedStack.setCount(vanillaMax);
-            return modifiedStack;
+            return vanillaMax;
         }
-        return stack;
+        return actualCount;
     }
 }
